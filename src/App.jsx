9b2328b6632d2589350be082a7c14cd04c3ac4b5
priya <priyadharshinird1415@gmail.com>
2026-05-24 01:08:29 +0530

    fix build issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Login from "./pages/Login"
-import Dashboard from "./pages/Dashboard"
+import Login from "./pages/Login.jsx"
+import Dashboard from "./pages/Dashboard.jsx"
 
 function App() {
   return (
     <BrowserRouter>
-
       <Routes>
-
         <Route path="/" element={<Login />} />
-
         <Route path="/dashboard" element={<Dashboard />} />
-
       </Routes>
-
     </BrowserRouter>
   )
 }
